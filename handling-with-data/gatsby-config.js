b7adad6a22764a30c987ba7818a9cbde59c85ef1
Rilton Franzone <riltonfranzone@gmail.com>
2020-05-16 12:15:14 -0300

    querying data with graphql

---
+++ handling-with-data/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Pandas eating lots'
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
