df0ce72a3d18b67086edee87d5262c75dcba5153
Rilton Franzone <riltonfranzone@gmail.com>
2020-05-16 11:21:18 -0300

    nested layout and navigation

---
+++ nested-layouts/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'gatsby'
+import Layout from '../components/layout'
+
+export default function AboutPage() {
+  return (
+    <Layout>
+      <h1>This is the about page</h1>
+      <p>Learning gatsby</p>
+    </Layout>
+  )
+}
